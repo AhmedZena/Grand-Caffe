0156b5951a93fcde7e2335beec469a108b140697
Ahmed Zena <87375061+AhmedZena@users.noreply.github.com>
2023-12-18 15:31:52 +0200

    some enhance in product

--- src/product/product.js
+++ src/product/product.js
@@ -32,6 +32,66 @@ for (productName in productsTypes) {
 
     let productPics = productsTypes[productName].pics;
     // console.log(productPics); // (3) ["https://seelaz.com/cdn/shop/products/TurkishCoffee.png?v=1669145044&width=713", "https://seelaz.com/cdn/shop/products/TURKISHCOffe.png?v=1669145044&width=713", "https://w7.pngwing.com/pngs/115/907/png-transparent-coffee-bag-exchange-food-paper-coffee-food-coffee-envase.png"]
+    console.log(productPics);
+
+    // attribute color be unique
+    let bgImages = document.querySelector(".bgImages");
+    bgImages.innerHTML = "";
+    for (let i = 0; i < productPics.length; i++) {
+      let pic = document.createElement("img");
+      pic.src = productPics[i];
+      pic.classList.add("shoe");
+      //   pic.setAttribute("color", productPics[i].split("=")[2].split("&")[0]);
+      if (i == 0) {
+        pic.classList.add("show");
+        pic.setAttribute("color", "orange");
+      } else if (i == 1) {
+        pic.setAttribute("color", "blue");
+      } else if (i == 2) {
+        pic.setAttribute("color", "red");
+      }
+      bgImages.appendChild(pic);
+    }
+
+    // share link to social media
+    // console.log(productsTypes[productName].shareLink);
+
+    //   <div class="title">
+    //   <label for="copyLinkInput" class="copyLinkLabel">Copy Link</label>
+    //  <input type="text" value="https://www.youtube.com/watch?v=7wtfhZwyrcc" id="copyLinkInput"
+    //      readonly>
+    // </div>
+
+    let shareLinkInput = document.querySelector("#copyLinkInput");
+    shareLinkInput.value = productsTypes[productName].shareLink;
+
+    // share icon
+    let shareIcon = document.querySelector(".shareIcon");
+
+    //label
+    let copyLinkLabel = document.querySelector(".copyLinkLabel");
+
+    shareIcon.addEventListener("click", function () {
+      copyToClipboard(shareLinkInput.value);
+    });
+
+    copyLinkLabel.addEventListener("click", function () {
+      copyToClipboard(shareLinkInput.value);
+    });
+
+    function copyToClipboard(text) {
+      shareLinkInput.select();
+      shareLinkInput.setSelectionRange(0, 99999);
+      navigator.clipboard.writeText(text);
+
+      // <span id="isLinkCopied"></span>
+      let linkCopied = document.querySelector("#isLinkCopied");
+      linkCopied.innerHTML = "Link Copied To Clipboard !";
+
+      setTimeout(function () {
+        linkCopied.innerHTML = "";
+      }, 2000);
+    }
 
     let productDescription = productsTypes[productName]["10-word-description"];
     // console.log(productDescription); // This is a coffee made in french so it is very good
@@ -49,8 +109,11 @@ for (productName in productsTypes) {
 
       //onclick
       subTypeDiv.addEventListener("click", function () {
-        console.log(this);
+        // console.log(this);
         // remove active class from all divs
+        // console.log(textDescription);
+        price.innerHTML = "choose product !";
+        textDescription.innerHTML = "";
         let allDivs = document.querySelectorAll(".roasting");
         allDivs.forEach((div) => div.classList.remove("active"));
         // add active class to the clicked div
@@ -62,7 +125,7 @@ for (productName in productsTypes) {
 
         // change weights when click on roasting get weights from the json in condition if the radio value equal the json value
         //for ( lastType in productSubTypes[ subType ].priceAndSize ) {
-        console.log(productSubTypes[radioValue].priceAndSize); // {small: {…}, medium: {…}, large: {…}}
+        // console.log(productSubTypes[radioValue].priceAndSize); // {small: {…}, medium: {…}, large: {…}}
         // remove the weights from the container
         weights.innerHTML = "";
         for (lastType in productSubTypes[radioValue].priceAndSize) {
@@ -76,8 +139,8 @@ for (productName in productsTypes) {
 
           //onclick
           weightDiv.addEventListener("click", function () {
-            console.log(productSubTypes[radioValue].priceAndSize);
-            console.log(this);
+            // console.log(productSubTypes[radioValue].priceAndSize);
+            // console.log(this);
             // remove active class from all divs
             let allDivs = document.querySelectorAll(".weight");
             allDivs.forEach((div) => div.classList.remove("active"));
@@ -86,10 +149,10 @@ for (productName in productsTypes) {
             // put the weight text in the container
             weightText.innerHTML = "";
             for (lastType in productSubTypes[radioValue].priceAndSize) {
-              console.log(this.innerHTML);
+              //   console.log(this.innerHTML);
 
               if (lastType == this.innerHTML) {
-                console.log("match");
+                // console.log("match");
                 //weightText.innerHTML = `This ${ this.innerHTML } coffee have a weight of ${ productSubTypes[ radioValue ].priceAndSize[ lastType ].size } gm and it cover ${ productSubTypes[ radioValue ].priceAndSize[ lastType ].cups }.`;
                 weightText.innerHTML = `This <span class="coloredWeight">${this.innerHTML}</span> coffee have a weight of <span class="coloredWeight">${productSubTypes[radioValue].priceAndSize[lastType].size} gm</span> and it cover <span class="coloredWeight">${productSubTypes[radioValue].priceAndSize[lastType].cups}</span>`;
               }
@@ -100,56 +163,51 @@ for (productName in productsTypes) {
             for (lastType in productSubTypes[radioValue].priceAndSize) {
               console.log(this.innerHTML);
               if (lastType == this.innerHTML) {
-                console.log("match");
+                console.log("matched buy");
                 price.innerHTML = `${productSubTypes[radioValue].priceAndSize[lastType].price} LE`;
-
                 buy.value =
                   productSubTypes[radioValue].priceAndSize[lastType].id;
-                console.log(buy);
+                // console.log(buy);
               }
             }
-            buy.addEventListener("click", function () {
-              console.log(buy.value);
-              //localStorage.setItem( "coffeeWeightId", buy.value );
-
-              // check if the id is in the local storage or not and also add all object to the local storage
-              //localStorage.setItem( "product", JSON.stringify( productSubTypes[ radioValue ].priceAndSize[ lastType ] ) );
-              //localStorage.setItem("chosenProducts", JSON.stringify( productSubTypes[ radioValue ].priceAndSize[ lastType ] ) );
-              // create arr like : {id: "c1t1v3s3", size: "500", price: 170, cups: "24 cups"}
-              // [{
-              //     "id": "c1t1v3s3",
-              //       "size": "500",
-              //         "price": 170,
-              //           "cups": "24 cups";
-              //  }]
-              let chosenProducts = [];
-              //let chosenProduct = {
-              //  "id": buy.value,
-              //"size": productSubTypes[ radioValue ].priceAndSize[ lastType ].size,
-              //"price": productSubTypes[ radioValue ].priceAndSize[ lastType ].price,
-              //"cups": productSubTypes[ radioValue ].priceAndSize[ lastType ].cups
-
-              //};
-
-              //let chosenProduct = productSubTypes[ radioValue ].priceAndSize[ lastType ];
-              let chosenProduct;
-              for (key in productSubTypes[radioValue].priceAndSize[lastType]) {
-                chosenProduct = {
-                  id: buy.value,
-                  [key]:
-                    productSubTypes[radioValue].priceAndSize[lastType][key],
-                  chosenNums: 1,
-                };
-              }
-              chosenProducts.push(chosenProduct);
-              console.log(chosenProducts);
-              //localStorage.setItem( "chosenProducts", JSON.stringify( chosenProducts ) );
-            });
           });
         }
+        buy.addEventListener("click", function () {
+          console.log(buy.value); // c1t1v3s2
+
+          for (lastproduct in productSubTypes[radioValue].priceAndSize) {
+            if (
+              productSubTypes[radioValue].priceAndSize[lastproduct].id ==
+              buy.value
+            ) {
+              console.log(
+                productSubTypes[radioValue].priceAndSize[lastproduct]
+              );
+
+              // get from local storage
+              let chosenProducts = localStorage.getItem("chosenProducts")
+                ? JSON.parse(localStorage.getItem("chosenProducts"))
+                : [];
+
+              console.log(chosenProducts);
+
+              // push the chosen product to the array
+              chosenProducts.push(
+                productSubTypes[radioValue].priceAndSize[lastproduct]
+              );
+
+              console.log(chosenProducts);
+              // save the array to local storage
+              localStorage.setItem(
+                "chosenProducts",
+                JSON.stringify(chosenProducts)
+              );
+            }
+          }
+        });
 
         // change sub description when click on roasting get sub description from the json in condition if the radio value equal the json value
-        console.log(productSubTypes[radioValue].description); // This is a coffee light made in french so it is very good.
+        // console.log(productSubTypes[radioValue].description); // This is a coffee light made in french so it is very good.
         // remove the sub description from the container
         textDescription.innerHTML = "";
         textDescription.innerHTML = productSubTypes[radioValue].description;
@@ -157,7 +215,7 @@ for (productName in productsTypes) {
         // remove the reviews from the container
         reviews.innerHTML = "";
         let productReviews = productSubTypes[radioValue].reviews;
-        console.log(productReviews); // (2) [{…}, {…}]
+        // console.log(productReviews); // (2) [{…}, {…}]
 
         for (let i = 0; i < productReviews.length; i++) {
           // console.log( productReviews[ i ] ); // {id: 'c1t3v4s3r1', name: 'John', review: 'This is an awesome coffee'} {id: 'c1t3v4s3r2', name: 'Emily', review: 'Loved the light flavor!'}
@@ -175,120 +233,120 @@ for (productName in productsTypes) {
           reviews.appendChild(reviewDiv);
         }
 
+        // // for stars
         // for stars
-
-        // remove the stars from the container
+        // Clear the existing stars
         stars.innerHTML = "";
-        let starsNumber = productSubTypes[radioValue].stars;
-        // want use i to create that number of stars
-        // check if have half star or not , if it have half star then add it to the stars number
-        let halfStar = false;
-        if (starsNumber % 1 != 0) {
-          halfStar = true;
-          starsNumber = Math.floor(starsNumber);
-        }
 
-        for (let i = 0; i < starsNumber; i++) {
-          //  console.log( i );
+        // Get the star rating from the product type
+        let starRating = productSubTypes[radioValue].stars;
+
+        // Calculate the number of full stars, half stars, and empty stars
+        let fullStars = Math.floor(starRating);
+        let halfStar = starRating % 1 !== 0;
+        let emptyStars = 5 - fullStars - (halfStar ? 1 : 0);
+
+        // Create and append full stars
+        for (let i = 0; i < fullStars; i++) {
           let star = document.createElement("i");
-          star.classList.add("fas");
-          star.classList.add("fa-star");
+          star.classList.add("fas", "fa-star");
           stars.appendChild(star);
         }
 
+        // Create and append half star if needed
         if (halfStar) {
           let star = document.createElement("i");
-          star.classList.add("fas");
-          star.classList.add("fa-star-half-alt");
+          star.classList.add("fas", "fa-star-half-alt");
+          stars.appendChild(star);
+        }
+
+        // Create and append empty stars
+        for (let i = 0; i < emptyStars; i++) {
+          let star = document.createElement("i");
+          star.classList.add("far", "fa-star");
           stars.appendChild(star);
         }
       });
 
       roastings.appendChild(subTypeDiv);
-
-      /*
-                        buy.addEventListener( "click", function () {
-                            let activeWeight = document.querySelector( ".weight.active" );
-
-                            for ( lastType in productSubTypes[ subType ].priceAndSize ) {
-                                //console.log( lastType );
-                                //   console.log( activeWeight.innerHTML );
-                                if ( productSubTypes[ subType ].priceAndSize[ lastType ].id == buy.value && lastType == activeWeight.innerHTML ) {
-                                    console.log( "match" );
-                                    console.log( productSubTypes[ subType ].priceAndSize[ lastType ].id );
-                                    //localStorage.setItem( "coffeeWeightId", productSubTypes[ subType ].priceAndSize[ lastType ].id );
-                                }
-                              }
-
-
-                        }
-                        
-                    
-                        );
-                        */
     }
-    const sizes = document.querySelectorAll(".size");
-    const colors = document.querySelectorAll(".color");
-    const shoes = document.querySelectorAll(".shoe");
-    const gradients = document.querySelectorAll(".gradient");
-    const shoeBg = document.querySelector(".shoeBackground");
-
-    let prevColor = "orange";
-    let animationEnd = true;
-
-    function changeSize() {
-      sizes.forEach((size) => size.classList.remove("active"));
-      this.classList.add("active");
-    }
-
-    function changeColor() {
-      if (!animationEnd) return;
-      let primary = this.getAttribute("primary");
-      let color = this.getAttribute("color");
-      let shoe = document.querySelector(`.shoe[color="${color}"]`);
-      let gradient = document.querySelector(`.gradient[color="${color}"]`);
-      let prevGradient = document.querySelector(
-        `.gradient[color="${prevColor}"]`
-      );
-
-      if (color == prevColor) return;
-
-      colors.forEach((c) => c.classList.remove("active"));
-      this.classList.add("active");
-
-      document.documentElement.style.setProperty("--primary", primary);
-
-      shoes.forEach((s) => s.classList.remove("show"));
-      shoe.classList.add("show");
-
-      gradients.forEach((g) => g.classList.remove("first", "second"));
-      gradient.classList.add("first");
-      prevGradient.classList.add("second");
-
-      prevColor = color;
-      animationEnd = false;
-
-      gradient.addEventListener("animationend", () => {
-        animationEnd = true;
-      });
-    }
-
-    sizes.forEach((size) => size.addEventListener("click", changeSize));
-    colors.forEach((c) => c.addEventListener("click", changeColor));
-
-    let x = window.matchMedia("(max-width: 1000px)");
-
-    function changeHeight() {
-      if (x.matches) {
-        let shoeHeight = shoes[0].offsetHeight;
-        shoeBg.style.height = `${shoeHeight * 0.9}px`;
-      } else {
-        shoeBg.style.height = "475px";
-      }
-    }
-
-    changeHeight();
-
-    window.addEventListener("resize", changeHeight);
   }
+  const sizes = document.querySelectorAll(".size");
+  const colors = document.querySelectorAll(".color");
+  const shoes = document.querySelectorAll(".shoe");
+  const gradients = document.querySelectorAll(".gradient");
+  const shoeBg = document.querySelector(".shoeBackground");
+
+  let prevColor = "orange";
+  let animationEnd = true;
+
+  function changeSize() {
+    sizes.forEach((size) => size.classList.remove("active"));
+    this.classList.add("active");
+  }
+
+  function changeColor() {
+    if (!animationEnd) return;
+    let primary = this.getAttribute("primary");
+    let color = this.getAttribute("color");
+    let shoe = document.querySelector(`.shoe[color="${color}"]`);
+    let gradient = document.querySelector(`.gradient[color="${color}"]`);
+    let prevGradient = document.querySelector(
+      `.gradient[color="${prevColor}"]`
+    );
+
+    if (color == prevColor) return;
+
+    colors.forEach((c) => c.classList.remove("active"));
+    this.classList.add("active");
+
+    document.documentElement.style.setProperty("--primary", primary);
+
+    shoes.forEach((s) => s.classList.remove("show"));
+    shoe.classList.add("show");
+
+    gradients.forEach((g) => g.classList.remove("first", "second"));
+    gradient.classList.add("first");
+    prevGradient.classList.add("second");
+
+    prevColor = color;
+    animationEnd = false;
+
+    gradient.addEventListener("animationend", () => {
+      animationEnd = true;
+    });
+  }
+
+  sizes.forEach((size) => size.addEventListener("click", changeSize));
+  colors.forEach((c) => c.addEventListener("click", changeColor));
+
+  let x = window.matchMedia("(max-width: 1000px)");
+
+  function changeHeight() {
+    if (x.matches) {
+      let shoeHeight = shoes[0].offsetHeight;
+      shoeBg.style.height = `${shoeHeight * 0.9}px`;
+    } else {
+      shoeBg.style.height = "475px";
+    }
+  }
+
+  changeHeight();
+
+  window.addEventListener("resize", changeHeight);
+}
+
+function firePopUp() {
+  console.log("firePopUp");
+  document.querySelector(".popup").classList.add("active");
+
+  // if you want to close the popup when clicking button x
+  document.querySelector(".close-btn").addEventListener("click", function () {
+    document.querySelector(".popup").classList.remove("active");
+  });
+
+  setTimeout(function () {
+    console.log("firePopUp");
+    document.querySelector(".popup").classList.remove("active");
+  }, 4000);
 }
