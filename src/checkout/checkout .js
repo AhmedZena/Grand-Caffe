59750d6cd32862660b4ea39865439e44c64bc8a1
Ahmed Zena <87375061+AhmedZena@users.noreply.github.com>
2023-12-20 19:47:02 +0200

    made checkout and sidebaar enhance

---
+++ src/checkout/checkout .js
@@ -0,0 +1,297 @@
+const btnBuy = document.getElementById("checkoutAndBuy");
+let shoppingcards = document.getElementById("shoppingcards");
+console.log(shoppingcards);
+var newEl;
+var numProd = 0;
+
+// get user from local storage
+// let users = JSON.parse(localStorage.getItem("users"));
+// let logedUserId = localStorage.getItem("logedUserId");
+// let user = users.find((user) => user.id == logedUserId);
+// console.log(user);
+function fetchData() {
+  shoppingcards.innerHTML = `<h3> Shopping cards</h3> `;
+
+  for (let i = 0; i < JSON.parse(localStorage.productsInCart).length; i++) {
+    createPrudects(i);
+  }
+}
+
+fetchData();
+totalPrice();
+
+function createPrudects(numProd) {
+  let dataPro = JSON.parse(localStorage.productsInCart)[numProd];
+
+  console.log(dataPro);
+
+  newEl = document.createElement("div");
+  newEl.className = `details pro pro${numProd}`;
+  newEl.id = `${dataPro.id}`;
+  shoppingcards.appendChild(newEl);
+
+  newEl = document.createElement("img");
+  newEl.src = dataPro.pics[2];
+  newEl.classList.add("imageCard");
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("h4");
+  newEl.innerHTML = dataPro.name;
+  newEl.className = "namePro";
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("p");
+  newEl.innerHTML = `<span class='p'>${dataPro.price}</span> E.L`;
+  newEl.className = "pricePro";
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("p");
+  newEl.innerHTML = `${dataPro.size}`;
+  newEl.className = "sizee";
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("button");
+  newEl.className = "plus";
+  newEl.innerHTML = `+`;
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("input");
+  newEl.className = "count";
+  newEl.value = dataPro.chosnNumber;
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("button");
+  newEl.className = "mins";
+  newEl.innerHTML = `-`;
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("button");
+  newEl.className = "remove";
+  newEl.classList.add("remove");
+  newEl.innerHTML = `Remove`;
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  newEl = document.createElement("hr");
+  document.getElementsByClassName(`pro${numProd}`)[0].appendChild(newEl);
+
+  numProd++;
+
+  if (numProd == 1) {
+    newEl = document.createElement("div");
+    newEl.className = "cardPro";
+    newEl.id = "cardPro";
+    shoppingcards.appendChild(newEl);
+
+    newEl = document.createElement("div");
+    newEl.className = "sum";
+    newEl.id = "sum";
+    document.getElementsByClassName("cardPro")[0].appendChild(newEl);
+
+    newEl = document.createElement("h4");
+    newEl.className = "subTotal";
+    newEl.innerHTML = "sub total";
+    document.getElementsByClassName("sum")[0].appendChild(newEl);
+
+    newEl = document.createElement("h4");
+    newEl.className = "tot";
+    newEl.id = "tot";
+
+    document.getElementById("sum").appendChild(newEl);
+  }
+}
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+
+shoppingcards.addEventListener("click", function (event) {
+  console.log("Clicked element:", event.target);
+
+  if (event.target.classList.contains("plus")) {
+    // Retrieve the array from local storage
+    let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+    for (let i = 0; i < cartItems.length; i++) {
+      if (cartItems[i].id == event.target.parentElement.id) {
+        cartItems[i].chosnNumber++;
+      }
+    }
+    // Store the modified array back in local storage
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    fetchData();
+    totalPrice();
+  } else if (event.target.classList.contains("mins")) {
+    let countElement = event.target.parentElement.querySelector(".count");
+    console.log(countElement);
+
+    // Retrieve the array from local storage
+    let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+    for (let i = 0; i < cartItems.length; i++) {
+      if (cartItems[i].id == event.target.parentElement.id) {
+        if (cartItems[i].chosnNumber > 1) {
+          cartItems[i].chosnNumber--;
+        } else {
+          cartItems.splice(i, 1);
+        }
+
+        if (cartItems.length == 0) {
+          localStorage.removeItem("productsInCart");
+          location.href = "../../index.html";
+        }
+      }
+    }
+
+    // Store the modified array back in local storage
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    fetchData();
+    totalPrice();
+
+    // Ensure count is not negative
+  } else if (event.target.classList.contains("remove")) {
+    // event.target.parentElement.remove();
+
+    // Retrieve the array from local storage
+    let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+    for (let i = 0; i < cartItems.length; i++) {
+      if (cartItems[i].id == event.target.parentElement.id) {
+        cartItems.splice(i, 1);
+      }
+      if (cartItems.length == 0) {
+        localStorage.removeItem("productsInCart");
+        location.href = "../../index.html";
+      }
+    }
+
+    // Store the modified array back in local storage
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    fetchData();
+
+    totalPrice();
+  } else if (event.target.classList.contains("checkOut")) {
+    // let user = JSON.parse(localStorage.logedUserId);
+    if (localStorage.getItem("logedUserId")) {
+      check();
+    } else {
+      location.href = "../loginReg/loginReg.html";
+    }
+  }
+});
+
+let count = document.getElementsByClassName("count"); // inputs array of numbers of products
+let priceSum = document.getElementsByClassName("p"); // array of prices of products
+
+// // console.log(price[0]);
+function totalPrice() {
+  // get array of products from local storage
+  let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+  // loop on them and get the price and the number of each product and add them to the total price
+  let sumPrice = 0;
+  for (let i = 0; i < cartItems.length; i++) {
+    sumPrice += cartItems[i].price * cartItems[i].chosnNumber;
+  }
+  console.log(sumPrice);
+  document.getElementsByClassName("tot")[0].innerHTML = `${sumPrice} E.L`;
+  localStorage.setItem("total", JSON.stringify(sumPrice));
+  return sumPrice;
+}
+
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+// -----------------------------------------------
+btnBuy.addEventListener("click", function () {
+  let balan = JSON.parse(localStorage.getItem("users"));
+  let total = JSON.parse(localStorage.getItem("total"));
+  //   console.log(balan[0].balance);
+  console.log(total);
+
+  //   if (balan[0].balance > total) {
+  //     alert("Done");
+  //   } else {
+  //     alert("Please charage your balance");
+  //     location.href = "../profile/profile.html";
+  //   }
+
+  // get users
+  let users = JSON.parse(localStorage.getItem("users"));
+  let logedUserId = localStorage.getItem("logedUserId");
+  let user = users.find((user) => user.id == logedUserId);
+  console.log(user);
+
+  if (user.balance > total) {
+    let confirm = window.confirm(
+      `Are you sure you want to buy these products?`
+    );
+    if (confirm) {
+      let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
+      user.balance = +user.balance - total;
+
+      if (user.prouctsBuyed) {
+        for (let i = 0; i < cartItems.length; i++) {
+          let found = user.prouctsBuyed.find(
+            (product) => product.id == cartItems[i].id
+          );
+          if (found) {
+            found.chosnNumber += cartItems[i].chosnNumber;
+          } else {
+            user.prouctsBuyed.push(cartItems[i]);
+          }
+        }
+      } else {
+        user.prouctsBuyed = cartItems;
+      }
+
+      localStorage.setItem("users", JSON.stringify(users));
+      localStorage.removeItem("productsInCart");
+      localStorage.removeItem("total");
+      location.href = "../profile/profile.html";
+    } else {
+      location.href = "../checkout/checkout.html";
+    }
+  } else {
+    // alert("Please charage your balance");
+    // location.href = "../profile/profile.html";
+
+    let confirm = window.confirm(
+      `Please charage your balance to buy these products`
+    );
+    if (confirm) {
+      location.href = "../profile/profile.html";
+    } else {
+      location.href = "../checkout/checkout.html";
+    }
+  }
+});
--- src/side/side.js
+++ src/side/side.js
@@ -10,13 +10,6 @@ var newEle;
 let dataFromlocal;
 
 function getData() {
-  // remove anything in the shopping cart
-  //   shopping.innerHTML = "";
-  // <h3> Shopping cards</h3>
-  //   <button id="close">
-  // <i class="fa fa-times"></i>
-  //   </button>;
-
   shopping.innerHTML = `<h3> Shopping cards</h3> <button id="close">x</button>`;
 
   close = document.getElementById("close");
@@ -123,12 +116,6 @@ function sidebar(numPro) {
   totalPrice();
 }
 
-// close.addEventListener("click", () => {
-//   console.log("clicked");
-//   shopping.classList.remove("openSide");
-//   container.classList.remove("openSide");
-// });
-
 shopping.addEventListener("click", function (event) {
   console.log("Clicked element:", event.target);
 
@@ -137,11 +124,6 @@ shopping.addEventListener("click", function (event) {
     shopping.classList.remove("openSide");
     container.classList.remove("openSide");
   } else if (event.target.classList.contains("plus")) {
-    // var countElement = event.target.parentElement.querySelector(".count");
-    // countElement.value = +countElement.value + 1;
-    totalPrice();
-    // console.log(countElement);
-
     // Retrieve the array from local storage
     let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
 
@@ -150,49 +132,38 @@ shopping.addEventListener("click", function (event) {
         cartItems[i].chosnNumber++;
       }
     }
+    // Store the modified array back in local storage
+    localStorage.setItem("productsInCart", JSON.stringify(cartItems));
+    getData();
+    totalPrice();
+  } else if (event.target.classList.contains("mins")) {
+    let countElement = event.target.parentElement.querySelector(".count");
+    console.log(countElement);
+
+    // Retrieve the array from local storage
+    let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
+
+    for (let i = 0; i < cartItems.length; i++) {
+      if (cartItems[i].id == event.target.parentElement.id) {
+        if (cartItems[i].chosnNumber > 1) {
+          cartItems[i].chosnNumber--;
+        } else {
+          cartItems.splice(i, 1);
+        }
+      }
+    }
 
     // Store the modified array back in local storage
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
     getData();
-  } else if (event.target.classList.contains("mins")) {
-    var countElement = event.target.parentElement.querySelector(".count");
-    countElement.value = +countElement.value - 1;
     totalPrice();
-    console.log(countElement);
-    if (countElement.value <= 0) {
-      //   event.target.parentElement.remove();
-      //   numPro--;
-      //   console.log(numPro);
-      //   if (numPro == 0) {
-      //     document.getElementsByClassName("cardPro")[0].remove();
-      //     localStorage.removeItem("productsInCart");
-      //   }
 
-      // Retrieve the array from local storage
-      let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
-
-      for (let i = 0; i < cartItems.length; i++) {
-        if (cartItems[i].id == event.target.parentElement.id) {
-          cartItems.splice(i, 1);
-        }
-      }
-
-      // Store the modified array back in local storage
-      localStorage.setItem("productsInCart", JSON.stringify(cartItems));
-      getData();
-      totalPrice();
-    }
     // Ensure count is not negative
   } else if (event.target.classList.contains("remove")) {
     // event.target.parentElement.remove();
 
     // Retrieve the array from local storage
     let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
-
-    // console.log(event.target.value);
-    // Remove the item at the specified index (numPro)
-    // cartItems.splice(event.target.value, 1);
-    // i want to loop on the array and remove the item that has the same id
 
     for (let i = 0; i < cartItems.length; i++) {
       if (cartItems[i].id == event.target.parentElement.id) {
@@ -204,12 +175,14 @@ shopping.addEventListener("click", function (event) {
     localStorage.setItem("productsInCart", JSON.stringify(cartItems));
     getData();
 
-    // numPro--;
-    // if (numPro == 0) {
-    //   document.getElementsByClassName("cardPro")[0].remove();
-    //   localStorage.removeItem("productsInCart");
-    // }
     totalPrice();
+  } else if (event.target.classList.contains("checkOut")) {
+    // let user = JSON.parse(localStorage.logedUserId);
+    if (localStorage.getItem("logedUserId")) {
+      check();
+    } else {
+      location.href = "../loginReg/loginReg.html";
+    }
   }
 });
 
@@ -218,62 +191,8 @@ let priceSum = document.getElementsByClassName("p"); // array of prices of produ
 
 // // console.log(price[0]);
 function totalPrice() {
-  //   var sumPrice = 0;
-
-  //   for (let x = 0; x < priceSum.length; x++) {
-  //     // Use innerHTML instead of value
-  //     sumPrice = +priceSum[x].innerHTML * count[x].value + sumPrice;
-  //   }
-  //   // console.log(sumPrice);
-  //   document.getElementsByClassName("total")[0].innerHTML = `${sumPrice} E.L`;
-  //   return sumPrice;
-
   // get array of products from local storage
   let cartItems = JSON.parse(localStorage.getItem("productsInCart")) || [];
-
-  /*
-[
-  {
-    "id": "c1t1v2s2",
-    "size": "250",
-    "price": 95,
-    "cups": "12 cups",
-    "pics": [
-      "https://www.tucanocoffee.com/wp-content/uploads/2020/04/3-1.jpg",
-      "https://seelaz.com/cdn/shop/products/TurkishCoffee.png?v=1669145044&width=713",
-      "https://seelaz.com/cdn/shop/products/turkishCoffe125.png?v=1669145044&width=713"
-    ],
-    "name": "french medium medium",
-    "chosnNumber": 3
-  },
-  {
-    "id": "c1t1v1s2",
-    "size": "250",
-    "price": 90,
-    "cups": "12 cups",
-    "pics": [
-      "https://www.tucanocoffee.com/wp-content/uploads/2020/04/3-1.jpg",
-      "https://seelaz.com/cdn/shop/products/TurkishCoffee.png?v=1669145044&width=713",
-      "https://seelaz.com/cdn/shop/products/turkishCoffe125.png?v=1669145044&width=713"
-    ],
-    "name": "french light medium",
-    "chosnNumber": 1
-  },
-  {
-    "id": "c1t1v4s1",
-    "size": "125",
-    "price": 65,
-    "cups": "6 cups",
-    "pics": [
-      "https://www.tucanocoffee.com/wp-content/uploads/2020/04/3-1.jpg",
-      "https://seelaz.com/cdn/shop/products/TurkishCoffee.png?v=1669145044&width=713",
-      "https://seelaz.com/cdn/shop/products/turkishCoffe125.png?v=1669145044&width=713"
-    ],
-    "name": "french extraDark small",
-    "chosnNumber": 1
-  }
-]
-*/
 
   // loop on them and get the price and the number of each product and add them to the total price
   let sumPrice = 0;
@@ -282,44 +201,11 @@ function totalPrice() {
   }
   console.log(sumPrice);
   document.getElementsByClassName("total")[0].innerHTML = `${sumPrice} E.L`;
+  localStorage.setItem("total", sumPrice);
   return sumPrice;
 }
 
-// // var id = document.getElementById("addTo").value;
-// // console.log(id[1]);
-// // var xhr = new XMLHttpRequest();
-// // xhr.open("get", "./data/categories.json");
-// // xhr.send();
-// // var data;
-// // xhr.addEventListener("load", function() {
-// //   if (xhr.status == 200) {
-// //     data = JSON.parse(xhr.response);
-// //     console.log(data.HotChocklate);
-// //   }
-
-// // })
-
-// // // var out
-// // // function getPro() {
-// // //   // Ensure data is an array and numPro is defined
-// // //   if (Array.isArray(data) && numPro !== undefined) {
-// // //      out = data.filter(x => {
-// // //       return x.id =(document.getElementById('addTo').value);
-// // //     });
-
-// // //     console.log(out);
-
-// // //   if (out.length > 0) {
-// // //     const priceElement = document.getElementsByClassName('price')[numPro];
-// // //     priceElement.innerHTML = `${out[0].price} E.L`; // Assuming price is a property in your data
-// // //   } else {
-// // //     console.log("Product not found");
-// // //   }
-// // // } else {
-// // //   console.log("Data is not an array or numPro is not defined");
-// // //   }00
-// // // console.log(document.getElementById('addTo').value);
-
-// // // console.log(input.valu)
-
-// // console.log(price);
+function check() {
+  location.href = "../checkout/checkout.html";
+  //   window.open("../checkout/checkout.html");
+}
